9bcad4beffdfb10d9fda3c28062a7e73040d605e
Ulrborg <92532312+Ulrborg@users.noreply.github.com>
2024-08-14 02:36:00 +0200

    #Commit 6? Add new things
    Added new msgbox style with new result action in code  (Not finished yet)
    
    Added new button (Not finished yet)

---
+++ modernUI_Button.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace NukUX_ModernUI
+{
+    public partial class modernUI_Button : UserControl
+    {
+        private Color _defaultBackColor = Color.MediumSlateBlue;
+        private Color _mouseEnterColor = Color.SlateBlue;
+        private Color _mouseDownColor = Color.DarkSlateBlue;
+        private Color _mouseLeaveColor = Color.MediumSlateBlue;
+        private Color _mouseUpColor = Color.SlateBlue;
+
+        public modernUI_Button()
+        {
+            this.Size = new Size(150, 40);
+            this.BackColor = _defaultBackColor;
+            this.ForeColor = Color.White;
+            this.DoubleBuffered = true;
+            this.Text = "Click me !"; // Default text
+
+            this.Paint += ModernUI_Button_Paint;
+            this.MouseEnter += ModernUI_Button_MouseEnter;
+            this.MouseLeave += ModernUI_Button_MouseLeave;
+            this.MouseDown += ModernUI_Button_MouseDown;
+            this.MouseUp += ModernUI_Button_MouseUp;
+        }
+
+        private void ModernUI_Button_Paint(object sender, PaintEventArgs e)
+        {
+            int borderRadius = 10; // Set border-radius to 5px
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(new Rectangle(0, 0, borderRadius, borderRadius), 180, 90);
+            path.AddArc(new Rectangle(this.Width - borderRadius, 0, borderRadius, borderRadius), -90, 90);
+            path.AddArc(new Rectangle(this.Width - borderRadius, this.Height - borderRadius, borderRadius, borderRadius), 0, 90);
+            path.AddArc(new Rectangle(0, this.Height - borderRadius, borderRadius, borderRadius), 90, 90);
+            path.CloseFigure();
+
+            this.Region = new Region(path);
+
+            // Draw background
+            using (SolidBrush brush = new SolidBrush(this.BackColor))
+            {
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                e.Graphics.FillPath(brush, path);
+            }
+
+            // Draw text centered
+            TextRenderer.DrawText(e.Graphics, this.ButtonText, this.Font, this.ClientRectangle, this.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+
+        private void ModernUI_Button_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = MouseEnterColor; // Change color on hover
+        }
+
+        private void ModernUI_Button_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = MouseLeaveColor; // Revert color when mouse leaves
+        }
+
+        private void ModernUI_Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.BackColor = MouseDownColor; // Change color on mouse down
+        }
+
+        private void ModernUI_Button_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.BackColor = MouseUpColor; // Revert color on mouse up
+        }
+
+        // Property to get or set the button text
+        [Category("Appearance")]
+        [Description("Sets the text displayed on the button.")]
+        public string ButtonText
+        {
+            get { return this.Text; }
+            set { this.Text = value; this.Invalidate(); } // Redraw to update text
+        }
+
+        // Properties to change the colors for different states
+        [Category("Appearance")]
+        [Description("Sets the background color when the mouse enters the button area.")]
+        public Color MouseEnterColor
+        {
+            get { return _mouseEnterColor; }
+            set { _mouseEnterColor = value; }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the background color when the mouse leaves the button area.")]
+        public Color MouseLeaveColor
+        {
+            get { return _mouseLeaveColor; }
+            set { _mouseLeaveColor = value; }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the background color when the mouse is pressed down on the button.")]
+        public Color MouseDownColor
+        {
+            get { return _mouseDownColor; }
+            set { _mouseDownColor = value; }
+        }
+
+        [Category("Appearance")]
+        [Description("Sets the background color when the mouse is released on the button.")]
+        public Color MouseUpColor
+        {
+            get { return _mouseUpColor; }
+            set { _mouseUpColor = value; }
+        }
+    }
+}
---
+++ modernUI_MsgBox.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NukUX_ModernUI
+{
+    public partial class modernUI_MsgBox : UserControl
+    {
+        public MsgBoxAction Action { get; private set; }
+
+        public modernUI_MsgBox()
+        {
+            
+
+            // Set default properties for the UserControl
+            this.Size = new Size(400, 200);
+            this.BackColor = Color.White;
+
+            // Message label
+            Label messageLabel = new Label
+            {
+                Dock = DockStyle.Top,
+                Height = 100,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Segoe UI", 12),
+                ForeColor = Color.Black,
+                Text = "Your message here"
+            };
+            this.Controls.Add(messageLabel);
+
+            // OK Button
+            Button okButton = new Button
+            {
+                Text = "OK",
+                BackColor = Color.MediumSlateBlue,
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                Size = new Size(100, 35),
+                Anchor = AnchorStyles.Bottom | AnchorStyles.Right
+            };
+            okButton.FlatAppearance.BorderSize = 0;
+            okButton.Location = new Point(this.Width - okButton.Width - 20, this.Height - okButton.Height - 20);
+            okButton.Click += OkButton_Click;
+            this.Controls.Add(okButton);
+
+            // Cancel Button
+            Button cancelButton = new Button
+            {
+                Text = "Cancel",
+                BackColor = Color.Gray,
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                Size = new Size(100, 35),
+                Anchor = AnchorStyles.Bottom | AnchorStyles.Right
+            };
+            cancelButton.FlatAppearance.BorderSize = 0;
+            cancelButton.Location = new Point(okButton.Left - cancelButton.Width - 10, okButton.Top);
+            cancelButton.Click += CancelButton_Click;
+            this.Controls.Add(cancelButton);
+
+            // Initialize action to cancel
+            Action = MsgBoxAction.Cancel;
+
+            // Resize event to reposition buttons
+            this.Resize += ModernUI_MsgBox_Resize;
+        }
+
+        private void ModernUI_MsgBox_Resize(object sender, EventArgs e)
+        {
+            // Reposition the buttons when the UserControl is resized
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    if (button.Text == "OK")
+                    {
+                        button.Location = new Point(this.Width - button.Width - 20, this.Height - button.Height - 20);
+                    }
+                    else if (button.Text == "Cancel")
+                    {
+                        button.Location = new Point(this.Width - button.Width * 2 - 30, this.Height - button.Height - 20);
+                    }
+                }
+            }
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            Action = MsgBoxAction.Ok;
+            this.ParentForm.DialogResult = DialogResult.OK;
+            this.ParentForm.Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Action = MsgBoxAction.Cancel;
+            this.ParentForm.DialogResult = DialogResult.Cancel;
+            this.ParentForm.Close();
+        }
+
+        public void SetMessage(string message)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is Label label)
+                {
+                    label.Text = message;
+                }
+            }
+        }
+
+        public static MsgBoxAction Show(string message, string title = "Message")
+        {
+            Form form = new Form
+            {
+                Text = title,
+                Size = new Size(400, 200),
+                FormBorderStyle = FormBorderStyle.None,
+                StartPosition = FormStartPosition.CenterScreen,
+                BackColor = Color.White,
+                ShowInTaskbar = false
+            };
+
+            modernUI_MsgBox msgBox = new modernUI_MsgBox();
+            msgBox.SetMessage(message);
+            form.Controls.Add(msgBox);
+
+            form.ShowDialog();
+
+            return msgBox.Action;
+        }
+    }
+}
